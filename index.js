74d544eedc15eace848512156eb4ea977a46dbea
whc2001 <ianwang0122@outlook.com>
2022-12-24 22:18:43 -0500

    test4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function XOR(dat, key) {
     let out = [];
     for(let i = 0; i < dat.length; i++) {
-        out.push(dat[i] ^ key[i % key.length]);
+        out.push(dat[i] ^ key);
     }
     return out;
 }
@@ -12,8 +12,6 @@ function main(args) {
     if(args[0]) {
         console.log(`Token: ${new Buffer(XOR(args[0], 0xA5)).toString('base64')}`);
     }
-    console.log(__dirname);
-    console.log(fs.existsSync("./output/"));
     const input = ~~fs.readFileSync('./raw/input.txt');
     if(input === 666) {
         console.log('Magic number detected. Exiting with error...');
@@ -22,6 +20,8 @@ function main(args) {
     else {
         const value = input + 1;
         console.log(`Writing ${value} to output.txt`);
+        if(!fs.existsSync('./output'))
+            fs.mkdirSync('./output');
         fs.writeFileSync('./output/output.txt', value.toString());
     }
 }
